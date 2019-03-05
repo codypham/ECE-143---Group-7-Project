26d0a303264e690ae3ea46060347f664ea2ec2d6
Huang Xinyu <554830906@qq.com>
2019-03-05 10:04:12 -0800

    Change xaxis of clustering map

--- clusters/GroupingPlot.py
+++ clusters/GroupingPlot.py
@@ -20,14 +20,16 @@ for i,j in states_dict.items():
     rev_dict[j] = i
 
 data = list_to_cluster
-
-#Using KMeans
+print(data)
+#生成一个随机数据，样本大小为100, 特征数为2（这里因为要画二维图，所以就将特征设为2，至于三维怎么画？
+#后续看有没有机会研究，当然你也可以试着降维到2维画图也行）
 estimator = KMeans(n_clusters=4)#构造聚类器，构造一个聚类数为3的聚类器
 estimator.fit(data)#聚类
 label_pred = estimator.labels_ #获取聚类标签
 centroids = estimator.cluster_centers_ #获取聚类中心
 inertia = estimator.inertia_ # 获取聚类准则的总和
 mark = ['or', 'ob', 'og', 'ok']
+#这里'or'代表中的'o'代表画圈，'r'代表颜色为红色，后面的依次类推
 color = 0
 j=0
 
@@ -38,7 +40,7 @@ for i in label_pred:
 
 plt.grid(True, lw = 2, ls = '--', c = '.75')
 plt.ylabel('Salary Per Month in USD')
-plt.ylabel('COST OF LIVING in Index (100 as Benchmark)')
+plt.xlabel('COST OF LIVING in Index (100 as Benchmark)')
 plt.title('Clustering States Based on Salary and Cost of Living')
 
 
