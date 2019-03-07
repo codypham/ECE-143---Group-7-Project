9621939dc557d1c08b58f3b9324881c97af1b079
Cody Pham <Cody@Codys-MacBook-Pro.local>
2019-03-07 09:10:54 -0800

    Comments to heatmap.py

--- visuals/heatmap.py
+++ visuals/heatmap.py
@@ -2,21 +2,27 @@ import plotly
 import plotly.plotly as py
 import pandas as pd
 
+# setup Plotly credentials
 plotly.tools.set_credentials_file(username='codypham', api_key='CDRkiQM17NLNcxyrmvsC')
 
+# read data set into Pandas DataFrame
 df = pd.read_csv('../data/data_analysis.tsv', sep='\t')
 
+# convert cost of living to strings
 for col in df.columns:
     df[col] = df[col].astype(str)
 
+# color scaling for interactive map
 scl = [[0.0, 'rgb(242,240,247)'],[0.2, 'rgb(218,218,235)'],[0.4, 'rgb(188,189,220)'],\
             [0.6, 'rgb(158,154,200)'],[0.8, 'rgb(117,107,177)'],[1.0, 'rgb(84,39,143)']]
 
+# html for interactive map
 df['text'] = df['STATE'] + '<br>' +\
     'Salary '+df['SALARY'] + '<br>'+\
     'Tax '+df['TAX']+'%<br>'+\
     'Cost of Living '+df['COST OF LIVING']
 
+# init map data
 data = [ dict(
         type='choropleth',
         colorscale = scl,
@@ -34,6 +40,7 @@ data = [ dict(
             title = "USD")
         ) ]
 
+# header layout
 layout = dict(
         title = 'Average Software Engineer Takehome Salary by State<br>(Adjusted for state COL)',
         geo = dict(
@@ -43,5 +50,6 @@ layout = dict(
             lakecolor = 'rgb(255, 255, 255)'),
              )
 
+# plot map into html file
 fig = dict( data=data, layout=layout )
 plotly.offline.plot( fig, filename='d3-cloropleth-map' )
